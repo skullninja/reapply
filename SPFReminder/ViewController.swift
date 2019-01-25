84b0891ccb941f74f3299890707a8cf81ceb06e7
Amber Reyngoudt <amber@Ambers-MacBook-Pro-2.local>
2019-01-24 20:28:59 -0800

    notification and timer countdown

--- SPFReminder/ViewController.swift
+++ SPFReminder/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 import ForecastIO
 import CoreLocation
+import UserNotifications
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var lblUVIndex: UILabel!
+    @IBOutlet weak var btnReminder: UIButton!
+    @IBOutlet weak var lblTimerCountdown: UILabel!
+    
     let locationManager = CLLocationManager()
     var uvIndexNeedsUpdate: Bool = true
+    
+    let center = UNUserNotificationCenter.current()
+    
+    var timer = Timer()
+    var isTimerRunning = false
+    //4800 is 80 minutes
+    var seconds = 4800
     
     lazy var client: DarkSkyClient = {
         let darkSky = DarkSkyClient(apiKey: "16d1cdbf343ab6a7ee0dcb340b7484ff")
@@ -73,5 +84,55 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
     }
 
+    @IBAction func setReminderNotification(_ sender: Any) {
+       
+        btnReminder.isEnabled = false
+        
+        center.getNotificationSettings{ (settings) in
+            if settings.authorizationStatus == .authorized {
+                // Notifications allowed
+                let content = UNMutableNotificationContent()
+                content.title = "Don't forget"
+                content.body = "Reapply sunblock"
+                //content.sound = UNNotificationSound.default()
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(self.seconds),
+                                                                repeats: false)
+                
+                let identifier = "UYLLocalNotification"
+                let request = UNNotificationRequest(identifier: identifier,
+                                                    content: content, trigger: trigger)
+                self.center.add(request, withCompletionHandler: { (error) in
+                    if let error = error {
+                        // Something went wrong
+                    }
+                })
+            }
+        }
+        
+        runTimer()
+    }
+    
+    func runTimer(){
+       timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if seconds == 0{
+            timer.invalidate()
+             btnReminder.isEnabled = true
+            lblTimerCountdown.text = "Sunburning!!!"
+            return
+        }
+        seconds -= 1
+        lblTimerCountdown.text = timeString(time: TimeInterval(seconds))
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
 }
 
