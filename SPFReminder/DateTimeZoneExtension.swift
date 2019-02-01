994a839fdc29aa0cfda5e707a4b47dcfc80d9108
Dave Peck <peckda@gmail.com>
2019-01-31 21:11:13 -0800

    Added Time Zone Extension

---
+++ SPFReminder/DateTimeZoneExtension.swift
@@ -0,0 +1,22 @@
+//
+//  DateTimeZoneExtension.swift
+//  SPFReminder
+//
+//  Created by Dave Peck on 1/31/19.
+//  Copyright © 2019 Skull Ninja Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+
+    func convertFromGMT(timeZone: TimeZone) -> Date {
+        let delta = TimeInterval(timeZone.secondsFromGMT())
+        return addingTimeInterval(delta)
+    }
+
+    func convertToTimeZone(initTimeZone: TimeZone, timeZone: TimeZone) -> Date {
+        let delta = TimeInterval(timeZone.secondsFromGMT() - initTimeZone.secondsFromGMT())
+        return addingTimeInterval(delta)
+    }
+}
