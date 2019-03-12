45448d26e3a723437fda3008c363da8336974095
Amber Reyngoudt <amber@Ambers-MacBook-Pro-2.local>
2019-03-12 16:45:51 -0700

    update to reminder sync

--- SPFReminder/Models/ReminderCloudKitModel.swift
+++ SPFReminder/Models/ReminderCloudKitModel.swift
@@ -23,7 +23,7 @@ class ReminderCloudKitModel{
             let reminderRecord = CKRecord(recordType: "Reminder")
             reminderRecord.setValue(content, forKey: "content")
             reminderRecord.setValue(start, forKey: "start")
-            
+           
             database.save(reminderRecord) { _, error in
                 guard error != nil else { return }
                 print("error: \(String(describing: error))")
@@ -32,7 +32,7 @@ class ReminderCloudKitModel{
         }
     }
     
-    func fetchReminders(_ resultsLimit: Int) -> [Reminder]{
+    func fetchReminders(_ resultsLimit: Int, completionHandler: @escaping ([Reminder]) ->()){
         
         let pred = NSPredicate(value: true)
         let query = CKQuery(recordType: "Reminder", predicate: pred)
@@ -56,43 +56,83 @@ class ReminderCloudKitModel{
         operation.queryCompletionBlock = { [unowned self] (cursor, error) in
             DispatchQueue.main.async {
                 if error == nil {
-                  print("no error")
+                    print("no error")
+                    completionHandler(newReminders)
                 } else {
                     print("Fetch failed. There was a problem fetching the list of reminders: \(error!.localizedDescription)")
                 }
             }
         }
         database.add(operation)
-        
-        return newReminders
     }
     
     func syncReminders() {
         
         var localReminders: [Reminder] = ReminderService.shared.loadAll()
-        var cloudKitReminders: [Reminder] = fetchReminders(50)
-        
-        //no reminders saved on the device. save any reminders from cloudkit to device.
-        if localReminders.count == 0 {
-            for reminder in cloudKitReminders{
-                ReminderService.shared.save(reminder)
+        var cloudKitReminders: [Reminder] = []
+            
+        fetchReminders(50, completionHandler: {newReminders in
+           cloudKitReminders = newReminders
+            
+            //no reminders saved on the device. save any reminders from cloudkit to device.
+            if localReminders.count == 0 {
+                for reminder in cloudKitReminders{
+                    ReminderService.shared.save(reminder)
+                }
+                return
             }
-            return
-        }
-        
-        //check if any reminder exist on the device and not in cloudkit
-        if localReminders.count > 0 {
             
-            localReminders.sort(by: { $0.start!.compare($1.start!) == .orderedDescending
-            })
-            
-            cloudKitReminders.sort(by: { $0.start!.compare($1.start!) == .orderedDescending
-            })
-            
-             for reminder in localReminders{
-                print(reminder.start ?? Date())
+            if cloudKitReminders.count == 0 {
+                for reminder in localReminders{
+                    self.add(reminder: reminder)
+                }
+                return
             }
-           
+            
+            //check if any reminders exist on the device and not in cloudkit and vice versa
+            if localReminders.count > 0 && cloudKitReminders.count > 0{
+                
+                localReminders.sort(by: { $0.start!.compare($1.start!) == .orderedDescending})
+                cloudKitReminders.sort(by: { $0.start!.compare($1.start!) == .orderedDescending})
+                
+                let firstCloudkitDate = cloudKitReminders[0].start
+                let firstLocalDate = localReminders[0].start
+                
+                if firstLocalDate!.compare(firstCloudkitDate!) ==  ComparisonResult.orderedDescending{
+                    for reminder in localReminders{
+                        print(reminder.start ?? Date())
+                    }
+                }
+            }
+        })
+        
+    }
+    
+    
+    //for testing
+    func cleanUpRecords() {
+    
+        database.fetchAllRecordZones { zones, error in
+            guard let zones = zones, error == nil else {
+                print("Error fetching zones.")
+                return
+            }
+    
+            let zoneIDs = zones.map { $0.zoneID }
+    
+            let deletionOperation = CKModifyRecordZonesOperation(recordZonesToSave: nil, recordZoneIDsToDelete: zoneIDs)
+    
+            deletionOperation.modifyRecordZonesCompletionBlock = { _, deletedZones, error in
+    
+                guard error == nil else {
+                    let error = error!
+                    print("Error deleting records.", error)
+                    return
+                }
+                print("Records successfully deleted in this zone.")
+            }
+            self.database.add(deletionOperation)
+            
         }
         
     }
