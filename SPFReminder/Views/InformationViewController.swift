c6f6a4ddf3e3aae1cf1722e30da6d8c000f9fca9
Dave Peck <peckda@gmail.com>
2019-04-22 22:22:23 -0700

    WIP Multi View Implementation

---
+++ SPFReminder/Views/InformationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InformationViewController.swift
+//  SPFReminder
+//
+//  Created by Dave Peck on 4/22/19.
+//  Copyright © 2019 Skull Ninja Inc. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+    
+}
