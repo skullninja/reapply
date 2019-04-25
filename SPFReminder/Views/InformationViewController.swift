65e5888cdbe80c5acc16099c1c22fbdd577a50eb
Amber Reyngoudt <amber@Ambers-MacBook-Pro-2.local>
2019-04-24 18:40:08 -0700

    working on learn section

--- SPFReminder/Views/InformationViewController.swift
+++ SPFReminder/Views/InformationViewController.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class InformationViewController: UIViewController {
     
+    @IBOutlet weak var btnSunBasics: UIButton!
+    @IBOutlet weak var btnTips: UIButton!
+    @IBOutlet weak var btnReviews: UIButton!
+    @IBOutlet weak var btnShop: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        btnSunBasics.layer.cornerRadius = 6
+        btnTips.layer.cornerRadius = 6
+        btnReviews.layer.cornerRadius = 6
+        btnShop.layer.cornerRadius = 6
+    }
+    
+    @IBAction func btnSunBasicsTapped(_ sender: Any) {
+        guard let url = URL(string: "https://amberreyn.wixsite.com/mysite-5/sun-care-basics") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func btnTipsTapped(_ sender: Any) {
+        guard let url = URL(string: "https://amberreyn.wixsite.com/mysite-5/sun-care-basics") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func btnReviewsTapped(_ sender: Any) {
+        guard let url = URL(string: "https://amberreyn.wixsite.com/mysite-5/sun-care-basics") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func btnShopTapped(_ sender: Any) {
+        guard let url = URL(string: "https://amberreyn.wixsite.com/mysite-5/sun-care-basics") else { return }
+        UIApplication.shared.open(url)
+    }
+    
 }
