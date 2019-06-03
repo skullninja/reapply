8e372e19d19cef7eaaa1ac728aa4838cdb707282
Amber Reyngoudt <amber@Ambers-MacBook-Pro-2.local>
2019-06-02 20:42:28 -0700

    onboarding UI stuff

--- SPFReminder/Views/OnboardingPageContent.swift
+++ SPFReminder/Views/OnboardingPageContent.swift
@@ -16,9 +16,13 @@ class OnboardingPageContent: UIViewController {
     
     @IBOutlet weak var btnNext: UIButton!
     @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var imgBackground: UIImageView!
+    @IBOutlet weak var lblWelcome: UILabel!
+    @IBOutlet weak var imgTitle: UIImageView!
     
     public var pageIndex = 0
     public var descriptionText: String = ""
+    public var imageName: String = ""
     
     weak var delegate:OnboardingPageContentDelegate?
     
@@ -26,6 +30,17 @@ class OnboardingPageContent: UIViewController {
         super.viewDidLoad()
         
         self.lblDescription.text = descriptionText
+        
+        let image : UIImage = UIImage(named:imageName)!
+        self.imgBackground.image = image
+        
+        if pageIndex > 0 {
+            lblWelcome.isHidden = true
+            imgTitle.isHidden = true
+        }else{
+            lblWelcome.isHidden = false
+            imgTitle.isHidden = false
+        }
         
         if(pageIndex == 1){
             btnNext.setTitle("Get Started!", for: .normal)
