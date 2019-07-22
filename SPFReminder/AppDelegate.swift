40fa80914490bccbb0c22f956436c319ac4ac8e0
Dave Peck <peckda@gmail.com>
2019-07-21 17:35:41 -0700

    Disable Firebase during debug

--- SPFReminder/AppDelegate.swift
+++ SPFReminder/AppDelegate.swift
@@ -19,7 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        #if DEBUG
+        Analytics.setAnalyticsCollectionEnabled(false)
+        #else
         FirebaseApp.configure()
+        #endif
         
         Fabric.with([Crashlytics.self])
         
