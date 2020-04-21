27b1d2fed162ef53d9c1f2695b89aac820b37748
Amber Reyngoudt <amberreyn@gmail.com>
2020-04-21 11:50:10 -0700

    news collection view, upgrade alamofire,image, Moya

---
+++ SPFReminder/Models/News.swift
@@ -0,0 +1,18 @@
+//
+//  News.swift
+//  SPFReminder
+//
+//  Created by Amber Reyngoudt on 4/21/20.
+//  Copyright © 2020 Skull Ninja Inc. All rights reserved.
+//
+
+import Foundation
+class News {
+    var id: String?
+    var date: String?
+    var title: String?
+    var url: String?
+    var source: String?
+    var author: String?
+    var imageUrl: String?
+}
---
+++ SPFReminder/Views/LearnViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LearnViewController.swift
+//  SPFReminder
+//
+//  Created by Amber Reyngoudt on 4/20/20.
+//  Copyright © 2020 Skull Ninja Inc. All rights reserved.
+//
+
+import UIKit
+
+class LearnViewController: GenericViewController, UICollectionViewDelegate {
+
+    @IBOutlet weak var learnCollectionView: UICollectionView!
+    
+    fileprivate let cellId = "newsCell"
+    
+    fileprivate var newsResults = [News]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.learnCollectionView.delegate = self
+        self.learnCollectionView.dataSource = self
+        
+        self.learnCollectionView.register(NewsCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        fetchData()
+    }
+    
+    func fetchData() {
+           
+           NewsService.shared.fetchNews{ (news, error) in
+               if let err = error {
+                   print("Failed to fetch launches:", err)
+                   //TO DO: display a nice error
+                   return
+               }
+               self.newsResults = news!
+               self.learnCollectionView.reloadData()
+           }
+       }
+}
+
+extension LearnViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return newsResults.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! NewsCollectionViewCell
+        let news = self.newsResults[indexPath.item] as News
+        cell.news =  news
+        return cell
+    }
+    
+    
+}
