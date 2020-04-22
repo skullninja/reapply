d9f467c22a79d13da0acc545cb0eab86cbf5abe5
Amber Reyngoudt <amberreyn@gmail.com>
2020-04-21 20:02:43 -0700

    sun basic cell layout stuff

--- SPFReminder/Views/Learn Section/SunBasicHorizontalCollectionView.swift
+++ SPFReminder/Views/Learn Section/SunBasicHorizontalCollectionView.swift
@@ -11,6 +11,8 @@ import UIKit
 private let reuseIdentifier = "horizontalCell"
 
 class SunBasicHorizontalCollectionView: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    var basics = [SunBasics]()
     
     let cellId = "cellId"
     
@@ -24,18 +26,50 @@ class SunBasicHorizontalCollectionView: UICollectionViewController, UICollection
     }
 
     override func viewDidLoad() {
-           super.viewDidLoad()
-           collectionView.backgroundColor = .white
+        super.viewDidLoad()
            
-           collectionView.register(SunBasicCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(SunBasicCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
            
-           if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
-               layout.scrollDirection = .horizontal
-           }
-       }
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        
+        self.collectionView.backgroundColor = .white
+       
+        setupData()
+    }
+    
+    func setupData(){
+        let basic1 = SunBasics()
+        basic1.title =  "What is SPF?"
+        basic1.url  = "https://www.reapplyapp.com/post/what-is-spf"
+        basic1.imageName =  "spf"
+        
+        let basic2 = SunBasics()
+        basic2.title =  "What is UV Index?"
+        basic2.url  = "https://www.reapplyapp.com/post/what-is-ultraviolet-index-uv-index"
+        basic2.imageName =  "sky"
+        
+        let basic3 = SunBasics()
+        basic3.title =  "What is UVA and UVB?"
+        basic3.url  = "https://www.reapplyapp.com/post/what-is-uva-uvb-rays"
+        basic3.imageName =  "spf"
+        
+        let basic4 = SunBasics()
+        basic4.title =  "Chemical or Mineral Sunscreens?"
+        basic4.url  = "https://www.reapplyapp.com/post/chemical-and-mineral-aka-physical-sunscreens"
+        basic4.imageName =  "spf"
+        
+        self.basics.append(basic1)
+        self.basics.append(basic2)
+        self.basics.append(basic3)
+        self.basics.append(basic4)
+        
+        self.collectionView.reloadData()
+    }
        
        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-           return .init(width: view.frame.width - 110, height: view.frame.height)
+           return .init(width: view.frame.width - 80, height: view.frame.height)
        }
        
        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -43,11 +77,14 @@ class SunBasicHorizontalCollectionView: UICollectionViewController, UICollection
        }
        
        override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-           return 3
+        return self.basics.count
        }
        
-       override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-           return cell
-       }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SunBasicCollectionViewCell
+        let basic = self.basics[indexPath.item] as SunBasics
+        cell.basic = basic
+        return cell
+    }
 }
